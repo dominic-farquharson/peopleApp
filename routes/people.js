bd57cc08e33542fd8c483a8e33b768d7c12eb6c9
Dominic Farquharson <farquharsondominic@gmail.com>
2017-03-19 21:45:43 -0400

    Created get/post route for new person

--- routes/people.js
+++ routes/people.js
@@ -8,5 +8,19 @@ router.get('/', function(req, res, next) {
   res.render('people/person', {title:'Person Route'})
 });
 
+/* Route for Creating a New Person */
+router.get('/new', (req, res, next) => {
+  res.send('new person');
+});
+
+/* Route for Posting New Person */
+router.post('/', (req, res, next) => {
+  res.send('posting new user');
+});
+
+
+/* Route for editing a person's info */
+
+
 // exporting router
 module.exports = router;
